f22585702e10378f22e6d2f19c7c0716525b0f49
Jairo <ja.fierro@uniandes.edu.co>
2024-04-18 19:31:07 -0500

    Cambios

--- main.ts
+++ main.ts
@@ -5,54 +5,80 @@ export const ap = series
 
 let seriesTable: HTMLElement = document.getElementById("infoSeries")!;
 
-let card: HTMLElement= document.getElementById('tarjeta-container')!;
 
 function mostrarDatosSeries(series:Serie[]){
 
     let total: number = 0;
     let tbodySeries=document.createElement("tbody");
+
+
     tbodySeries.innerHTML = '<thead><tr> <th scope="col">#</th> <th scope="col">Name</th> <th scope="col">Channel</th><th scope="col">Seasons</th></tr></thead>'
-    tbodySeries.innerHTML+= '<tbody>'
-    for (let i = 0; i < series.length; i++) {
-        const serie = series[i];
+
+
+    for (let serie of series) {
         total+=serie.seasons
-        tbodySeries.innerHTML += `
-            <tr class="table-active">
-                <th scope="row">${serie.numero}</th>
-                <td style="color: rgba(11,114,172,255);" >${serie.name}</td>
-                <td>${serie.channel}</td>
-                <td>${serie.seasons}</td> </tr>`
-        tbodySeries.addEventListener("click", function(){
-            mostrarTarjetas(i);
-        });
-        }
+        let trElement: HTMLElement = document.createElement("tr");
+        trElement.classList.add("table-active");
+
+        // Crear los elementos td para cada campo de la serie
+        let indexTd = document.createElement("td");
+        indexTd.textContent = serie.numero.toString();
+
+        let nameTd = document.createElement("td");
+        nameTd.style.color = "rgba(11,114,172,255)"; 
+        nameTd.textContent = serie.name;
+        // Agregar evento click al nombre de la serie
+        nameTd.addEventListener("click", () => mostrarTarjetas(serie));
+
+        let channelTd = document.createElement("td");
+        channelTd.textContent = serie.channel;
+
+        let seasonsTd = document.createElement("td");
+        seasonsTd.textContent = serie.seasons.toString();
+
+        // Agregar los elementos td al trElement
+        trElement.appendChild(indexTd);
+        trElement.appendChild(nameTd);
+        trElement.appendChild(channelTd);
+        trElement.appendChild(seasonsTd);
+
+        // Agregar trElement al tbody
+        tbodySeries.appendChild(trElement);
+        
+    }
     const average = total / series.length;
-    tbodySeries.innerHTML += `
-        <tr>
-            <td colspan="4" style="text-align: left;">Seasons average: ${average}</td>
-        </tr>`;
-    tbodySeries.innerHTML+= '</tbody>'
+
+
+    let trSeason = document.createElement("tr");
+    trSeason.textContent = "Seasons average:" + " " + average.toString();
+    tbodySeries.appendChild(trSeason);
+
 
     seriesTable.appendChild(tbodySeries);
 }
 
 
-function mostrarTarjetas(i:number){
+function mostrarTarjetas(serie:Serie){
     const card = `
     <div class="card">
         <div class="card-body" style="width: 18rem;">
-            <img class="card-img-top" src="${series[i].image}">
+            <img class="card-img-top" src="${serie.image}">
             <div class="card-body">
-            <h5 class="card-title">${series[i].name}</h5>
-            <p class="card-text">${series[i].description}</p>
-            <a href="${series[i].urlInformation}" class="card-link">${series[i].urlInformation}</a>
+            <h5 class="card-title">${serie.name}</h5>
+            <p class="card-text">${serie.description}</p>
+            <a href="${serie.urlInformation}" class="card-link">${serie.urlInformation}</a>
           </div>
         </div>
     </div>`;
- document.getElementById('tarjeta-container')!.innerHTML = card;
+
+    let cardContainer = document.getElementById("tarjeta-container");
+    if (!cardContainer) {
+        cardContainer = document.createElement("div");
+        cardContainer.id = "cardContainer";
+        document.body.appendChild(cardContainer);
+    }
+    cardContainer.innerHTML = card;
 }
 console.log(ap)
-
-console.log("hola mundo")
 
 mostrarDatosSeries(series);
--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,53 @@
 import { series } from './data.js';
 export var ap = series;
 var seriesTable = document.getElementById("infoSeries");
-var card = document.getElementById('tarjeta-container');
 function mostrarDatosSeries(series) {
     var total = 0;
     var tbodySeries = document.createElement("tbody");
     tbodySeries.innerHTML = '<thead><tr> <th scope="col">#</th> <th scope="col">Name</th> <th scope="col">Channel</th><th scope="col">Seasons</th></tr></thead>';
-    tbodySeries.innerHTML += '<tbody>';
-    var _loop_1 = function (i) {
-        var serie = series[i];
+    var _loop_1 = function (serie) {
         total += serie.seasons;
-        tbodySeries.innerHTML += "\n            <tr class=\"table-active\">\n                <th scope=\"row\">".concat(serie.numero, "</th>\n                <td style=\"color: rgba(11,114,172,255);\" >").concat(serie.name, "</td>\n                <td>").concat(serie.channel, "</td>\n                <td>").concat(serie.seasons, "</td> </tr>");
-        tbodySeries.addEventListener("click", function () {
-            mostrarTarjetas(i);
-        });
+        var trElement = document.createElement("tr");
+        trElement.classList.add("table-active");
+        // Crear los elementos td para cada campo de la serie
+        var indexTd = document.createElement("td");
+        indexTd.textContent = serie.numero.toString();
+        var nameTd = document.createElement("td");
+        nameTd.style.color = "rgba(11,114,172,255)";
+        nameTd.textContent = serie.name;
+        // Agregar evento click al nombre de la serie
+        nameTd.addEventListener("click", function () { return mostrarTarjetas(serie); });
+        var channelTd = document.createElement("td");
+        channelTd.textContent = serie.channel;
+        var seasonsTd = document.createElement("td");
+        seasonsTd.textContent = serie.seasons.toString();
+        // Agregar los elementos td al trElement
+        trElement.appendChild(indexTd);
+        trElement.appendChild(nameTd);
+        trElement.appendChild(channelTd);
+        trElement.appendChild(seasonsTd);
+        // Agregar trElement al tbody
+        tbodySeries.appendChild(trElement);
     };
-    for (var i = 0; i < series.length; i++) {
-        _loop_1(i);
+    for (var _i = 0, series_1 = series; _i < series_1.length; _i++) {
+        var serie = series_1[_i];
+        _loop_1(serie);
     }
     var average = total / series.length;
-    tbodySeries.innerHTML += "\n        <tr>\n            <td colspan=\"4\" style=\"text-align: left;\">Seasons average: ".concat(average, "</td>\n        </tr>");
-    tbodySeries.innerHTML += '</tbody>';
+    var trSeason = document.createElement("tr");
+    trSeason.textContent = "Seasons average:" + " " + average.toString();
+    tbodySeries.appendChild(trSeason);
     seriesTable.appendChild(tbodySeries);
 }
-function mostrarTarjetas(i) {
-    var card = "\n    <div class=\"card\">\n        <div class=\"card-body\" style=\"width: 18rem;\">\n            <img class=\"card-img-top\" src=\"".concat(series[i].image, "\">\n            <div class=\"card-body\">\n            <h5 class=\"card-title\">").concat(series[i].name, "</h5>\n            <p class=\"card-text\">").concat(series[i].description, "</p>\n            <a href=\"").concat(series[i].urlInformation, "\" class=\"card-link\">").concat(series[i].urlInformation, "</a>\n          </div>\n        </div>\n    </div>");
-    document.getElementById('tarjeta-container').innerHTML = card;
+function mostrarTarjetas(serie) {
+    var card = "\n    <div class=\"card\">\n        <div class=\"card-body\" style=\"width: 18rem;\">\n            <img class=\"card-img-top\" src=\"".concat(serie.image, "\">\n            <div class=\"card-body\">\n            <h5 class=\"card-title\">").concat(serie.name, "</h5>\n            <p class=\"card-text\">").concat(serie.description, "</p>\n            <a href=\"").concat(serie.urlInformation, "\" class=\"card-link\">").concat(serie.urlInformation, "</a>\n          </div>\n        </div>\n    </div>");
+    var cardContainer = document.getElementById("tarjeta-container");
+    if (!cardContainer) {
+        cardContainer = document.createElement("div");
+        cardContainer.id = "cardContainer";
+        document.body.appendChild(cardContainer);
+    }
+    cardContainer.innerHTML = card;
 }
 console.log(ap);
 console.log("hola mundo");
